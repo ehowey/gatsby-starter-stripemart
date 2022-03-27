fabaa208061bfab9c3d856c93223dd349d4fcd7e
ehowey <ehowey@gmail.com>
2022-03-26 21:39:11 -0600

    Fix footer

--- web/src/components/layout/footer/footer.js
+++ web/src/components/layout/footer/footer.js
@@ -16,12 +16,6 @@ const SiteFooter = () => {
         py: 3,
         mt: 5,
         gridArea: "footer",
-        display: "flex",
-        flexDirection: ["column-reverse", "row", null, null, null],
-
-        alignItems: ["left", "center", null, null, null],
-        gap: [4, 3, null, null, null],
-        justifyContent: "space-between",
         a: {
           color: "footer.links",
           ":hover, :active, :focus": {
@@ -31,11 +25,6 @@ const SiteFooter = () => {
       }}
     >
       <div>
-        <Themed.p sx={{ my: 0, fontSize: [4, 4, 4, 4, 4] }}>
-          <Themed.a href="https://www.instagram.com/west.wind.wool/">
-            <FiInstagram />
-          </Themed.a>
-        </Themed.p>
         <Themed.p sx={{ m: 0, fontSize: [1, 1, 1, 1, 1] }}>
           © {new Date().getFullYear()} {title}
         </Themed.p>
@@ -53,19 +42,6 @@ const SiteFooter = () => {
             Privacy
           </Themed.a>
         </Themed.p>
-      </div>
-      <div sx={{ mb: [4, 0, null, null, null] }}>
-        <img
-          src="/yarn-heart.svg"
-          alt="ball of wool with a heart"
-          width="180"
-          height="60"
-          sx={{
-            height: "60px",
-            width: "180px",
-            aspectRatio: "attr(width) / attr(height)",
-          }}
-        />
       </div>
     </footer>
   )
